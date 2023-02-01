1e2662d79d3b2d38377b80fe6368051ec8f33035
Hernani Fernandes <hernanif1@gmail.com>
2023-02-01 11:44:39 +0100

    add template

---
+++ src/State.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let color = 'red';
+    let showText = true
+    const toggle = () => {
+        color = color === 'red' ? 'blue' : 'red';
+        showText = !showText
+    };
+</script>
+
+{#if showText}
+<h1 style="color: {color}">Selected color is {color}</h1>
+{:else}
+<p>Show text variable is {showText}</p>
+{/if}
+<button on:click={toggle}>toggle color</button>
