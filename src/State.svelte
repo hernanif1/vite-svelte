1dba470fe57672508df0cd47cbb9e3b99dafc7c0
Hernani Fernandes <hernanif1@gmail.com>
2023-02-01 11:40:34 +0100

    each

---
+++ src/State.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    const products = [
+        { id: "a1a2", name: 'Svelte' },
+        { id: "b1b2", name: 'React' },
+        { id: "c1c2", name: 'Vue' },
+    ];
+</script>
+
+{#each products as product, i}
+    <div>index: {i}, ID: {product.id}, name: {product.name}</div>
+{/each}
